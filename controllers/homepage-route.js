a466a45fbf8a452e2e10be1e398d2ce4a481b3d7
castarke <candler.starke@yahoo.com>
2023-05-06 15:34:59 -0400

    created a homepage route and homepage handlebars. Will probably need adjustments

---
+++ controllers/homepage-route.js
@@ -0,0 +1,76 @@
+const router = require('express').Router();
+const { Movie, User } = require('../models');
+
+router.get('/', async (req, res) => {
+    try{  
+        // console.log('homepage route reached')
+        // const response = await
+        // fetch('http://localhost:3001/api/post')
+        // const postData = await response.json()
+        const movieData = await Movie.findAll({
+            attributes:[
+                'id',
+                'title',
+                'genre',
+                'format',
+                'watched',
+                'img',
+                'description',
+                'rating'
+            ],            
+        });
+
+        const movie = movieData.map( singleMovie => singleMovie.get({plain: true}))
+        // console.log(post)
+        // res.status(200).json(post)
+        // console.log('post: ', post)
+        // res.status(200).json(postData)
+        res.render('homepage',{
+            movie, 
+            login: req.session.login
+        })
+    } catch(err){
+        console.log('There was an Error')
+        console.log(err)
+        res.status(500).json(err)
+    }
+});
+
+router.get('/movie/:id', async (req,res)=>{
+
+    console.log(req.params.id)
+    try{
+        const movieData = await Movie.findByPk(req.params.id, {
+            include: [
+                {
+                    model:Movie,
+                    attributes: ['title'],
+                }
+            ]
+        }
+        )
+        const movie = movieData.get({plain:true});
+
+        res.render('edit-movie', {
+            ...movie,
+            login: req.session.login
+        });
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.login) {
+      res.redirect('/');
+      return;
+    }
+    const login = req.session.login
+    res.render('login', login);
+  });
+
+  router.get('/signup', (req,res)=>{
+    res.render('login');
+  });
+
+  module.exports = router;
